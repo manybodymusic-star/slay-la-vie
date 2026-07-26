c3421b4b7b29a05b9ec86e9990c28aacac5a9d48
manybodymusic-star <manybodymusic@gmail.com>
2026-07-26 12:25:00 -0700

    Add files via upload

--- data.js
+++ data.js
@@ -2046,7 +2046,7 @@ window.resourceTileData = [
             "Woodwork unlocks are not required to fuel hot air balloons"
         ],
         "headers": ["", "I", "II", "III", "IV", "V", "VI", "VII"],
-        "columnWidths": [130, 92, 92, 92, 116, 108, 108, 92],
+        "columnWidths": [130, 92, 76, 92, 116, 108, 116, 92],
         "rows": 
         [
             [
